e2ef34b6e9a95dfb2098d5ee42f315d81ac0f339
xiyuchuanshuo <511263942@qq.com>
2015-03-24 20:09:33 +0800

    主界面更改 Signed-off-by: xiyuchuanshuo <511263942@qq.com>

--- app/src/main/java/com/kuoyu/earlywarningsystem/MainActivity.java
+++ app/src/main/java/com/kuoyu/earlywarningsystem/MainActivity.java
@@ -1,16 +1,24 @@
 package com.kuoyu.earlywarningsystem;
 
+import android.graphics.Color;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewConfiguration;
 import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
@@ -18,218 +26,236 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends ActionBarActivity implements OnClickListener,
-		OnPageChangeListener
-{
+        OnPageChangeListener {
 
-	private ViewPager mViewPager;
-	private List<Fragment> mTabs = new ArrayList<Fragment>();
-	private String[] mTitles = new String[]
-	{ "First Fragment !", "Second Fragment !", "Third Fragment !",
-			"Fourth Fragment !" };
-	private FragmentPagerAdapter mAdapter;
+    private Toolbar toolbar;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private ListView lvLeftMenu;
+    private String[] lvs = {"List Item 01", "List Item 02", "List Item 03", "List Item 04"};
+    private ArrayAdapter arrayAdapter;
+    private ImageView ivRunningMan;
+    private AnimationDrawable mAnimationDrawable;
 
-	private List<ChangeColorIconWithText> mTabIndicators = new ArrayList<ChangeColorIconWithText>();
+    private ViewPager mViewPager;
+    private List<Fragment> mTabs = new ArrayList<>();
+    private String[] mTitles = new String[]
+            {"First Fragment !", "Second Fragment !", "Third Fragment !",
+                    "Fourth Fragment !", "Five Fragment !"};
+    private FragmentPagerAdapter mAdapter;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		setOverflowButtonAlways();
+    private List<ChangeColorIconWithText> mTabIndicators = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setOverflowButtonAlways();
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Toolbar");//设置Toolbar标题
+        toolbar.setTitleTextColor(Color.parseColor("#ffffff")); //设置标题颜色
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true); //设置返回键可用
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
-		initView();
-		initDatas();
-		mViewPager.setAdapter(mAdapter);
-		initEvent();
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.dl_left);
+        lvLeftMenu = (ListView) findViewById(R.id.lv_left_menu);
 
-	}
+        //创建返回键，并实现打开关/闭监听
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.open, R.string.close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
 
-	/**
-	 * 初始化所有事件
-	 */
-	private void initEvent()
-	{
+            }
 
-		mViewPager.setOnPageChangeListener(this);
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
 
-	}
+            }
+        };
+        mDrawerToggle.syncState();
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+//设置菜单列表
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, lvs);
+        lvLeftMenu.setAdapter(arrayAdapter);
 
-	private void initDatas()
-	{
-		for (String title : mTitles)
-		{
-			TabFragment tabFragment = new TabFragment();
-			Bundle bundle = new Bundle();
-			bundle.putString(TabFragment.TITLE, title);
-			tabFragment.setArguments(bundle);
-			mTabs.add(tabFragment);
-		}
 
-		mAdapter = new FragmentPagerAdapter(getSupportFragmentManager())
-		{
+        initView();
+        initDatas();
+        mViewPager.setAdapter(mAdapter);
+        initEvent();
 
-			@Override
-			public int getCount()
-			{
-				return mTabs.size();
-			}
+    }
 
-			@Override
-			public Fragment getItem(int position)
-			{
-				return mTabs.get(position);
-			}
-		};
-	}
+    /**
+     * 初始化所有事件
+     */
+    private void initEvent() {
 
-	private void initView()
-	{
-		mViewPager = (ViewPager) findViewById(R.id.id_viewpager);
+        mViewPager.setOnPageChangeListener(this);
 
-		ChangeColorIconWithText one = (ChangeColorIconWithText) findViewById(R.id.id_indicator_one);
-		mTabIndicators.add(one);
-		ChangeColorIconWithText two = (ChangeColorIconWithText) findViewById(R.id.id_indicator_two);
-		mTabIndicators.add(two);
-		ChangeColorIconWithText three = (ChangeColorIconWithText) findViewById(R.id.id_indicator_three);
-		mTabIndicators.add(three);
-		ChangeColorIconWithText four = (ChangeColorIconWithText) findViewById(R.id.id_indicator_four);
-		mTabIndicators.add(four);
+    }
 
-		one.setOnClickListener(this);
-		two.setOnClickListener(this);
-		three.setOnClickListener(this);
-		four.setOnClickListener(this);
+    private void initDatas() {
+        for (String title : mTitles) {
+            TabFragment tabFragment = new TabFragment();
+            Bundle bundle = new Bundle();
+            bundle.putString(TabFragment.TITLE, title);
+            tabFragment.setArguments(bundle);
+            mTabs.add(tabFragment);
+        }
 
-		one.setIconAlpha(1.0f);
+        mAdapter = new FragmentPagerAdapter(getSupportFragmentManager()) {
 
-	}
+            @Override
+            public int getCount() {
+                return mTabs.size();
+            }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
-	{
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+            @Override
+            public Fragment getItem(int position) {
+                return mTabs.get(position);
+            }
+        };
+    }
 
-	private void setOverflowButtonAlways()
-	{
-		try
-		{
-			ViewConfiguration config = ViewConfiguration.get(this);
-			Field menuKey = ViewConfiguration.class
-					.getDeclaredField("sHasPermanentMenuKey");
-			menuKey.setAccessible(true);
-			menuKey.setBoolean(config, false);
-		} catch (Exception e)
-		{
-			e.printStackTrace();
-		}
-	}
+    private void initView() {
+        mViewPager = (ViewPager) findViewById(R.id.id_viewpager);
 
-	/**
-	 * 设置menu显示icon
-	 */
-	@Override
-	public boolean onMenuOpened(int featureId, Menu menu)
-	{
+        ChangeColorIconWithText one = (ChangeColorIconWithText) findViewById(R.id.id_indicator_one);
+        mTabIndicators.add(one);
+        ChangeColorIconWithText two = (ChangeColorIconWithText) findViewById(R.id.id_indicator_two);
+        mTabIndicators.add(two);
+        ChangeColorIconWithText three = (ChangeColorIconWithText) findViewById(R.id.id_indicator_three);
+        mTabIndicators.add(three);
+        ChangeColorIconWithText four = (ChangeColorIconWithText) findViewById(R.id.id_indicator_four);
+        mTabIndicators.add(four);
+        ChangeColorIconWithText five = (ChangeColorIconWithText) findViewById(R.id.id_indicator_five);
+        mTabIndicators.add(five);
+        one.setOnClickListener(this);
+        two.setOnClickListener(this);
+        three.setOnClickListener(this);
+        four.setOnClickListener(this);
+        five.setOnClickListener(this);
 
-		if (featureId == Window.FEATURE_ACTION_BAR && menu != null)
-		{
-			if (menu.getClass().getSimpleName().equals("MenuBuilder"))
-			{
-				try
-				{
-					Method m = menu.getClass().getDeclaredMethod(
-							"setOptionalIconsVisible", Boolean.TYPE);
-					m.setAccessible(true);
-					m.invoke(menu, true);
-				} catch (Exception e)
-				{
-					e.printStackTrace();
-				}
-			}
-		}
+        one.setIconAlpha(1.0f);
 
-		return super.onMenuOpened(featureId, menu);
-	}
+    }
 
-	@Override
-	public void onClick(View v)
-	{
-		clickTab(v);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
-	}
+    private void setOverflowButtonAlways() {
+        try {
+            ViewConfiguration config = ViewConfiguration.get(this);
+            Field menuKey = ViewConfiguration.class
+                    .getDeclaredField("sHasPermanentMenuKey");
+            menuKey.setAccessible(true);
+            menuKey.setBoolean(config, false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	/**
-	 * 点击Tab按钮
-	 * 
-	 * @param v
-	 */
-	private void clickTab(View v)
-	{
-		resetOtherTabs();
+    /**
+     * 设置menu显示icon
+     */
+    @Override
+    public boolean onMenuOpened(int featureId, Menu menu) {
 
-		switch (v.getId())
-		{
-		case R.id.id_indicator_one:
-			mTabIndicators.get(0).setIconAlpha(1.0f);
-			mViewPager.setCurrentItem(0, false);
-			break;
-		case R.id.id_indicator_two:
-			mTabIndicators.get(1).setIconAlpha(1.0f);
-			mViewPager.setCurrentItem(1, false);
-			break;
-		case R.id.id_indicator_three:
-			mTabIndicators.get(2).setIconAlpha(1.0f);
-			mViewPager.setCurrentItem(2, false);
-			break;
-		case R.id.id_indicator_four:
-			mTabIndicators.get(3).setIconAlpha(1.0f);
-			mViewPager.setCurrentItem(3, false);
-			break;
-		}
-	}
+        if (featureId == Window.FEATURE_ACTION_BAR && menu != null) {
+            if (menu.getClass().getSimpleName().equals("MenuBuilder")) {
+                try {
+                    Method m = menu.getClass().getDeclaredMethod(
+                            "setOptionalIconsVisible", Boolean.TYPE);
+                    m.setAccessible(true);
+                    m.invoke(menu, true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
-	/**
-	 * 重置其他的TabIndicator的颜色
-	 */
-	private void resetOtherTabs()
-	{
-		for (int i = 0; i < mTabIndicators.size(); i++)
-		{
-			mTabIndicators.get(i).setIconAlpha(0);
-		}
-	}
+        return super.onMenuOpened(featureId, menu);
+    }
 
-	@Override
-	public void onPageScrolled(int position, float positionOffset,
-			int positionOffsetPixels)
-	{
-		// Log.e("TAG", "position = " + position + " ,positionOffset =  "
-		// + positionOffset);
-		if (positionOffset > 0)
-		{
-			ChangeColorIconWithText left = mTabIndicators.get(position);
-			ChangeColorIconWithText right = mTabIndicators.get(position + 1);
-			left.setIconAlpha(1 - positionOffset);
-			right.setIconAlpha(positionOffset);
-		}
+    @Override
+    public void onClick(View v) {
+        clickTab(v);
 
-	}
+    }
 
-	@Override
-	public void onPageSelected(int position)
-	{
-		// TODO Auto-generated method stub
+    /**
+     * 点击Tab按钮
+     *
+     * @param v
+     */
+    private void clickTab(View v) {
+        resetOtherTabs();
 
-	}
+        switch (v.getId()) {
+            case R.id.id_indicator_one:
+                mTabIndicators.get(0).setIconAlpha(1.0f);
+                mViewPager.setCurrentItem(0, false);
+                break;
+            case R.id.id_indicator_two:
+                mTabIndicators.get(1).setIconAlpha(1.0f);
+                mViewPager.setCurrentItem(1, false);
+                break;
+            case R.id.id_indicator_three:
+                mTabIndicators.get(2).setIconAlpha(1.0f);
+                mViewPager.setCurrentItem(2, false);
+                break;
+            case R.id.id_indicator_four:
+                mTabIndicators.get(3).setIconAlpha(1.0f);
+                mViewPager.setCurrentItem(3, false);
+                break;
+            case R.id.id_indicator_five:
+                mTabIndicators.get(4).setIconAlpha(1.0f);
+                mViewPager.setCurrentItem(4, false);
+                break;
+        }
+    }
 
-	@Override
-	public void onPageScrollStateChanged(int state)
-	{
-		// TODO Auto-generated method stub
+    /**
+     * 重置其他的TabIndicator的颜色
+     */
+    private void resetOtherTabs() {
+        for (int i = 0; i < mTabIndicators.size(); i++) {
+            mTabIndicators.get(i).setIconAlpha(0);
+        }
+    }
 
-	}
+    @Override
+    public void onPageScrolled(int position, float positionOffset,
+                               int positionOffsetPixels) {
+        // Log.e("TAG", "position = " + position + " ,positionOffset =  "
+        // + positionOffset);
+        if (positionOffset > 0) {
+            ChangeColorIconWithText left = mTabIndicators.get(position);
+            ChangeColorIconWithText right = mTabIndicators.get(position + 1);
+            left.setIconAlpha(1 - positionOffset);
+            right.setIconAlpha(positionOffset);
+        }
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+        // TODO Auto-generated method stub
+
+    }
 
 }
